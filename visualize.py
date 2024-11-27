94a5269e9fb415ee1455e4828550ff2083eb2d95
Yifan Chen <117072842+yifan-ch@users.noreply.github.com>
2024-11-27 12:54:15 +0100

    started framework setup

--- visualize.py
+++ visualize.py
@@ -1,2 +1,13 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("data/parkinsons_data.csv")
+features = df.columns[1:]  # .tolist()
+
+
+def plot(df, feature):
+    print(df[feature])
+
+
+plot(df, features[0])
