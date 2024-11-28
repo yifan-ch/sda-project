5de7760a553f3894715899bbfe39659e57bb2269
Yifan Chen <117072842+yifan-ch@users.noreply.github.com>
2024-11-28 16:41:31 +0100

    fix: visualization now working properly again

--- data_model.py
+++ data_model.py
@@ -1,5 +1,6 @@
 import pandas as pd
-from env import DATA_PATH
+from pathlib import Path
+from env import DATA_GENERATED_PATH
 
 
 class Data:
@@ -11,15 +12,15 @@ class Data:
         self.df = pd.read_csv(path)
         self.features = self.df.columns
 
-    def filter_status(self, status):
-        return self.df.loc[self.df["status"] == status]
+        self.status_0 = self.df.loc[self.df["status"] == 0]
+        self.status_1 = self.df.loc[self.df["status"] == 1]
 
-    def filter_feature(self, feature):
-        return self.df[feature]
+    # def filter_feature(self, feature):
+    #     return self.df[feature]
 
     def display(self, df):
         print(df.to_string())
 
 
-data_raw = Data(f"{DATA_PATH}/generated/parkinsons_raw.csv")
-data_mean = Data(f"{DATA_PATH}/parkinsons_mean.csv")
+data_raw = Data(Path(DATA_GENERATED_PATH) / "parkinsons_raw.csv")
+data_mean = Data(Path(DATA_GENERATED_PATH) / "parkinsons_mean.csv")
--- generate_visualization.py
+++ generate_visualization.py
@@ -7,24 +7,29 @@ from env import RESULTS_VISUALIZATION_PATH
 # if path doesnt exist, create all missing folders
 Path(RESULTS_VISUALIZATION_PATH).mkdir(parents=True, exist_ok=True)
 
+
 data = data_model.data_mean
 
 
 def plot(data, feature, k=30):
+    # skip meaningless stats
+    if feature in ("id", "experiment", "status"):
+        return
+
     bins = np.linspace(data.df[feature].min(), data.df[feature].max(), k)
 
     plt.hist(
-        [data.filter_status(0), data.filter_status(1)],
+        [data.status_0[feature], data.status_1[feature]],
         bins=bins,
         label=["Healthy (status=0)", "Parkinson's patient (status=1)"],
     )
 
     plt.title(feature)
     plt.legend()
-    plt.savefig(Path(RESULTS_VISUALIZATION_PATH) / feature)
+    plt.savefig(Path(RESULTS_VISUALIZATION_PATH) / feature.replace(":", "-"))
     plt.clf()
 
 
 if __name__ == "__main__":
-    for f in data.features:
-        plot(data.filter_status(0), data.filter_status(1), f)
+    for feature in data.features:
+        plot(data, feature)
